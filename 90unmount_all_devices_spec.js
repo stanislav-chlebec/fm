c2f8fad2c54e81655cbfc794cd10a5a0b488989b
Stanislav Chlebec <schlebec@frinx.io>
2020-02-06 15:15:50 +0100

    fix undefined have no length attribute

--- 90unmount_all_devices_spec.js
+++ 90unmount_all_devices_spec.js
@@ -14,16 +14,10 @@ describe('Unmount all mounted devices', function() {
     //wait a second for finishing of loading of the list of connected devices
     //there is two xhr we will wait for and after then 3 times bunch of xhrs
     cy.wait(['@getAllStatusCli', '@getAllStatusNetconf']).then((xhrs) => {
-      //const cliDev = xhrs[0].responseBody.topology[0].node
-      //const netconfDev = xhrs[1].responseBody.topology[0].node
-      //cy.get('table tbody tr td:first-child', {timeout:5000}).should('have.length', cliDev.length + netconfDev.length)
-    })
-    cy.get('@getAllStatusCli').then((xhr1) => {
-      var cliDev = xhr1.response.body.topology[0].node
-      cy.get('@getAllStatusNetconf').then((xhr2) => {
-        const netconfDev = xhr2.response.body.topology[0].node
-        cy.get('table tbody tr td:first-child').should('have.length', cliDev.length + netconfDev.length)
-      })
+      const cliDev = xhrs[0].responseBody.topology[0].node
+      const netconfDev = xhrs[1].responseBody.topology[0].node
+      const rowCount = ((cliDev === undefined) ? 0 : cliDev.length) + ((netconfDev === undefined) ? 0 : netconfDev.length)
+      cy.get('table tbody tr td:first-child', {timeout:5000}).should('have.length', rowCount)
     })
 
     cy.get('table tbody tr td:first-child').click({multiple:true})
