c736b75c4a5bd3911439d9c66e533526a1379174
Stanislav Chlebec <schlebec@frinx.io>
2020-02-06 15:14:18 +0100

    speed up graphviz

--- 33lldp_graphviz_check_spec.js
+++ 33lldp_graphviz_check_spec.js
@@ -8,13 +8,26 @@ describe('Collect LLDP Information from Devices and Build Topology', function() 
     cy.wait(1000)
     cy.readFile('cypress/fixtures/lldp_export.json').then((json) => {
       expect(json).to.be.an('object')
+
       cy.get('textarea.ace_text-input').clear({force:true})
       cy.get('textarea.ace_text-input').invoke('show').type('{selectall}{backspace}',{force:true})
       cy.get('textarea.ace_text-input').invoke('show').type('{selectall}{del}',{force:true})
+
       console.log(json.export.output.export)
-      cy.get('textarea.ace_text-input').invoke('show').type(json.export.output.export.replace(/\\n/g, ''),{force:true})
-      //Cypress.$('textarea.ace_text-input').val(json.export.output.export)
-      //Cypress.$('textarea.ace_text-input').del
+	    
+      //https://github.com/cypress-io/cypress/issues/1123
+      cy.get('textarea.ace_text-input').invoke('show').type(json.export.output.export.replace(/\\n/g, ''),{force:true, delay: 0})
+
+      // I tried hard but nothing work only that above
+      //const myValue = json.export.output.export.replace(/\\n/g, '')
+      //console.log(myValue)
+      //cy.get('textarea.ace_text-input').then($span => {
+      //      $span.val('really long text');
+      //});	   
+      //cy.get('textarea.ace_text-input').clear({force:true}).trigger('blur',{force:true})
+      //cy.get('textarea.ace_text-input').invoke('val', myValue).trigger('change',{force:true})
+      //Cypress.$('textarea.ace_text-input').val(json.export.output.export).keypress()
+      //cy.get('textarea').invoke('val', myValue').trigger('change')
     })
   })
 })
