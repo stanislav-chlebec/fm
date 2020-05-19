81e6e58e5724db32acc78ef66f7c894022954d17
Stanislav Chlebec <schlebec@frinx.io>
2020-05-19 08:44:55 +0200

    fixing tests

--- 70unmount_all_devices_spec.js
+++ 70unmount_all_devices_spec.js
@@ -6,14 +6,14 @@ describe('Unmount all mounted devices', function() {
   it('unmounts all devices', function() { 
     cy.server()
     cy.route('/api/odl/oper/all/status/cli').as('getAllStatusCli')
-    cy.route('/api/odl/oper/all/status/topology-netconf').as('getAllStatusNetconf')
+    cy.route('/api/odl/oper/all/status//topology-netconf').as('getAllStatusNetconf')
 
     cy.visit('/') 
     cy.contains('UniConfig').click()	  
 
     //20200514
-    //this works on slower networks but not locally
-    //when running fm on localhost then these xhrs are too fast to catch
+    //THIS WORKS ON SLOWER NETWORKS BUT NOT LOCALLY
+    //WHEN RUNNING FM ON localhost THEN THESE XHRs ARE TOO FAST TO CATCH
     //wait a second for finishing of loading of the list of connected devices
     //there is two xhr we will wait for and after then 3 times bunch of xhrs
     //cy.wait(['@getAllStatusCli', '@getAllStatusNetconf']).then((xhrs) => {
@@ -22,6 +22,7 @@ describe('Unmount all mounted devices', function() {
     //  const rowCount = ((cliDev === undefined) ? 0 : cliDev.length) + ((netconfDev === undefined) ? 0 : netconfDev.length)
     //  cy.get('table tbody tr td:first-child', {timeout:5000}).should('have.length', rowCount)
     //})
+    //THIS SOMEHOW ALSO TENDS TO FAIL
     cy.waitForXHR('@getAllStatusCli', '@getAllStatusNetconf')
     //REPLACE WITH SOMETHING LESS PRONE TO FAIL
     //cy.get('table tbody tr:nth-child(10)').should('to.exist')
